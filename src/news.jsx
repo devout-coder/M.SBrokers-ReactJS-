04691101d8595c553fb5f2c341a791bb2eae0523
MANIKSHARMA2000 <115614705+MANIKSHARMA2000@users.noreply.github.com>
2022-12-29 21:06:19 +0530

    8

--- src/news.jsx
+++ src/news.jsx
@@ -1,25 +1,25 @@
  let count = 0
  const News = ({news}) =>{
     return(
-        <>
-        <h1 className="outlook">Today's Outlook</h1>
-        <ul className="newscontainer">
-            {news?.map((item) => {
-            return (
-            <li className="newsItem" key={count++}>
-                <div className="newsimage" >
-                    <img className="newsIMG" src={item.imageUrl} alt=""  />
-                </div>
-                <div className="feed">
-                    <h3>{item?.title}</h3>
-                    <p>{`${item?.author} | ${item.date} ${item?.time}`}</p>
-                    <p>{item?.content} <a className="newslink" href={item?.readMoreUrl}>See More..</a></p>
-                </div>
-            </li>)}
-            )}
-        </ul>
+        <div className="newsbox">
+            <h1 className="outlook">Today's Outlook</h1>
+            <ul className="newscontainer">
+                {news?.map((item) => {
+                return (
+                <li className="newsItem" key={count++}>
+                    <div className="newsimage" >
+                        <img className="newsIMG" src={item.imageUrl} alt=""  />
+                    </div>
+                    <div className="feed">
+                        <h3>{item?.title}</h3>
+                        <p>{`${item?.author} | ${item.date} ${item?.time}`}</p>
+                        <p>{item?.content} <a className="newslink" href={item?.readMoreUrl}>See More..</a></p>
+                    </div>
+                </li>)}
+                )}
+            </ul>
 
-        </>
+        </div>
     )
 }
 export default News
