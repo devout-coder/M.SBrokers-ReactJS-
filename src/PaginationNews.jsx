e952d8e9b41a2e56431ce04898201dcd05713a3a
MANIKSHARMA2000 <115614705+MANIKSHARMA2000@users.noreply.github.com>
2022-12-29 21:19:31 +0530

    4

--- src/PaginationNews.jsx
+++ src/PaginationNews.jsx
@@ -6,10 +6,10 @@ export const PagesNews = ({nextNewsPage,previousNewsPage}) => {
   return (
     <>
     <div className='news-btn'>
-        <button onClick = {()=>{
+        <button className='prev' onClick = {()=>{
            previousNewsPage()
         }}> <i className="fa-sharp fa-solid fa-chevron-up fa-2xl"></i></button>    
-        <button onClick = {()=>{
+        <button className='next' onClick = {()=>{
           nextNewsPage()
         }}><i className="fa-sharp fa-solid fa-chevron-down fa-2xl"></i></button>    
     </div>
