654b5ca37416dae09f2697c82d3a8d0d53bcb323
MANIKSHARMA2000 <115614705+MANIKSHARMA2000@users.noreply.github.com>
2022-12-30 15:02:43 +0530

    v4.5

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -6,20 +6,20 @@ export const Footer = () => {
         <div className="footer">
         
         <div className="icons">
-            <a  className="footer-icons" href=""><i className="fa-solid fa-arrow-trend-up fa-2xl"></i></a>
-            <a className="footer-icons" href=""><i className="fa-sharp fa-solid fa-coins fa-2xl"></i></a>
-            <a className="footer-icons" href=""><i className="fa-brands fa-facebook fa-2xl"></i></a>
-            <a className="footer-icons" href=""><i className="fa-brands fa-twitter fa-2xl"></i></a>
-            <a className="footer-icons" href=""><i className="fa-solid fa-id-card-clip fa-2xl"></i></a>
+            <a  className="footer-icons" href="https://in.tradingview.com/"><i className="fa-solid fa-arrow-trend-up fa-2xl"></i></a>
+            <a className="footer-icons" href="https://www.binance.com/en"><i className="fa-sharp fa-solid fa-coins fa-2xl"></i></a>
+            <a className="footer-icons" href="https://www.facebook.com/tradingview/"><i className="fa-brands fa-facebook fa-2xl"></i></a>
+            <a className="footer-icons" href="https://twitter.com/in_tradingview"><i className="fa-brands fa-twitter fa-2xl"></i></a>
+            <a className="footer-icons" href="https://twitter.com/linkedin"><i className="fa-solid fa-id-card-clip fa-2xl"></i></a>
         </div>
      
         <div className="links">
-            <a id="firstlink" className="footer-links"  href="">HOME</a>
+            <a id="firstlink" className="footer-links"  href="https://soft-panda-db902f.netlify.app">HOME</a>
             <a className="footer-links" target="_blank" href="https://www.nseindia.com/">NSE</a>
             <a className="footer-links" target="_blank" href="https://www.bseindia.com/">BSE</a>
-            <a className="footer-links" target="_blank" href="">MCX</a>
+            <a className="footer-links" target="_blank" href="https://www.mcxindia.com/">MCX</a>
             <a className="footer-links" target="_blank" href="https://www.binance.com/en">CRYPTO</a>
-            <a className="footer-links" href="">CARRERS</a>
+
         </div>
            
             <div className="copyright">
