de51fb7bd7f6ad70b850fd7249bdc3614ae3bfcc
MANIKSHARMA2000 <115614705+MANIKSHARMA2000@users.noreply.github.com>
2022-12-31 21:39:23 +0530

    v4.5

--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -6,7 +6,7 @@ export default function SearchBar({filter,RenderType}){
                 <button onClick={()=>RenderType('Advance')} >Advance</button>
                 <button onClick={()=>RenderType('TopGainers')} >Top 10 Gainers</button>
                 <button onClick={()=>RenderType('TopLoosers')} >Top 10 Loosers</button>
-                <button onClick={()=>RenderType('Decline')} >Decline</button>
+                <button className="decline-btn" onClick={()=>RenderType('Decline')} >Decline</button>
 
             </div>
             <br></br>
